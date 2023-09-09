ee5e10e30454392586135cc3ccfa2c839201f9b0
Emmett Miller <emmett.miller@sky.uk>
2023-09-09 12:37:45 +0100

    [FIX] Improve TypeScript configuration and package.json
    - Update `tsconfig.json` to enable declaration and declarationMap options, set outDir option, and enable esModuleInterop and forceConsistentCasingInFileNames options
    - Add `index.d.ts` file exporting modules from codeowners and parser directories and the substring type
    - Add `index.ts` file to export codeowners and substring modules
    - Remove ignore rule for `.d.ts` files in `.gitignore`
    - Update version in `package.json` to 1.0.7 and add types field pointing to dist/index.d.ts
    - Modify co:build script to use "npx tsc" and add co:declarations script for generating TypeScript declarations.

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+// Exporting from `codeowners` directory
+export * from './codeowners/index';
+export * from './codeowners/AddLineVisitor';
+
+// Exporting from the `parser` directory inside `codeowners`
+export * from './codeowners/parser/CODEOWNERSLexer';
+export * from './codeowners/parser/CODEOWNERSParser';
+export * from './codeowners/parser/CODEOWNERSParserListener';
+export * from './codeowners/parser/CODEOWNERSParserVisitor';
+
+// Exporting the `substring` type
+export * from './substring';
